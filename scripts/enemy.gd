e4a1d99376b29e9040c6cbed8a8e609b394fe644
Kayuusha <66058606+Kayuusha@users.noreply.github.com>
2024-06-01 10:40:20 -0300

    Add files via upload

---
+++ scripts/enemy.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+
+@onready var anim = $anim
+
+func _ready():
+	var enemy_types = anim.sprite_frames.get_animation_names()
+	anim.play(enemy_types[randi() % enemy_types.size()])
+
+func _on_visible_screen_exited():
+	queue_free()
